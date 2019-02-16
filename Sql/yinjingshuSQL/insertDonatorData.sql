120cce61f89904970067dc2c9f6ec8923d80c931
dihonoredGit <846299897@qq.com>
2019-02-16 20:14:54 +0800

    助印经书后端

---
+++ Sql/yinjingshuSQL/insertDonatorData.sql
@@ -0,0 +1,10 @@
+insert into Donatorinfo (wechatID,scriptureName,donateNum) values('a','金刚经',100);
+insert into Donatorinfo (wechatID,scriptureName,donateNum) values('b','心经',100);
+insert into Donatorinfo (wechatID,scriptureName,donateNum) values('c','首楞严经',100);
+insert into Donatorinfo (wechatID,scriptureName,donateNum) values('d','妙法莲华经',100);
+insert into Donatorinfo (wechatID,scriptureName,donateNum) values('e','无量寿经',100);
+insert into Donatorinfo (wechatID,scriptureName,donateNum) values('f','妙法莲华经',100);
+insert into Donatorinfo (wechatID,scriptureName,donateNum) values('g','无量寿经',100);
+insert into Donatorinfo (wechatID,scriptureName,donateNum) values('h','大般涅磐经',100);
+insert into Donatorinfo (wechatID,scriptureName,donateNum) values('i','妙法莲华经',100);
+insert into Donatorinfo (wechatID,scriptureName,donateNum) values('j','大般涅磐经',100);
