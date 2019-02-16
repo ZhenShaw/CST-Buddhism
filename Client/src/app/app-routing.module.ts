54463eb8aa86dc94ebde8bee03e3ac66a318ec82
ZHB <1074550119@qq.com>
2019-02-16 15:50:36 +0800

    创建助印经书页面和路由

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { GongfoComponent } from './components/gongfo/gongfo.component';
 import { XiuxingComponent } from './components/xiuxing/xiuxing.component';
 import { GongdeComponent } from './components/gongde/gongde.component';
 import { XindeComponent } from './components/xiuxing/xinde/xinde.component'
-
+import { ZhuyinjingshuComponent } from './components/gongde/zhuyinjingshu/zhuyinjingshu.component'
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -18,7 +18,8 @@ const routes: Routes = [
   { path: 'xiuxing', component: XiuxingComponent },
   { path: 'gongde', component: GongdeComponent },
   // 子页面路由写在下方
-  { path: 'xinde', component:XindeComponent}
+  { path: 'xinde', component:XindeComponent},
+  { path: 'gongde/zhuyinjingshu', component:ZhuyinjingshuComponent}
 ];
 
 @NgModule({
