08578226eb53208af3b52262832d282b855cbce1
2226210789 <2226210789@qq.com>
2019-02-16 21:29:13 +0800

    换佛

--- Client/src/app/components/gongfo/gongfo.component.ts
+++ Client/src/app/components/gongfo/gongfo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';//路由
+import axios from 'axios';//引入第三方模块进行数据请求
 
 @Component({
   selector: 'app-gongfo',
@@ -8,24 +8,65 @@ import { Router } from '@angular/router';//路由
 })
 export class GongfoComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor() { }
+
+  status:number
+  fo_list:any={
+    url:"http://api10.buddhaheart.cn/webfiles/sys/201710/171015001922d66c.png",
+    name:'',
+    details:''
+  }
+ 
+  xiang_list:any={
+    url:"http://api10.buddhaheart.cn/webfiles/sys/201710/171013142225d175.png",
+    name:'',
+    details:''
+  }
+
+  flower_list:any={
+    url:"http://api10.buddhaheart.cn/webfiles/sys/201710/1710142350470a8e.png",
+    name:'',
+    details:''
+  }
+
+  fruit_list:any={
+    url:"http://api10.buddhaheart.cn/webfiles/sys/201710/171013142539eb07.png",
+    name:'',
+    details:''
+  }
 
   ngOnInit() {
   }
 
   hua(){
-    this.router.navigateByUrl("/gongfo/dialog")
+    
   }
 
   fo(){
-   this.router.navigateByUrl("/gongfo/dialog")
+    this.status=1;
+    var url ="/fo"
+    axios({
+      method: 'post',
+      url: url,
+      data: {
+      },
+      headers: { "Content-Type": "application/x-www-form-urlencoded" }
+
+    }).then((res) => {
+      console.log(res.data)
+    })
+
   }
 
   xiang(){
-    this.router.navigateByUrl("/gongfo/dialog")
+    this.status=2;
   }
 
   fruit(){
-    this.router.navigateByUrl("/gongfo/dialog")
+    
+  }
+
+  bless(){
+
   }
 }
