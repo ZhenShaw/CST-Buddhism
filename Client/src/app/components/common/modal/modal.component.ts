6cfd28aa0438e7d7472b0b7c750fd089b6f93dee
ZhenShaw <1614570038@qq.com>
2019-02-16 12:57:22 +0800

    增加公共弹窗组件

---
+++ Client/src/app/components/common/modal/modal.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    this.open()
+  }
+
+  open() {
+    document.getElementById('modal').style.display = 'block'
+    document.getElementById('overlay').style.display = 'block'
+  }
+
+  close() {
+    document.getElementById('modal').style.display = 'none'
+    document.getElementById('overlay').style.display = 'none'
+  }
+
+}
