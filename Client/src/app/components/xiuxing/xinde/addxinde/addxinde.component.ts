1003e75ab4c3c1e57ec8c8bc9f3ffd577fe237cd
Mo <641878107@qq.com>
2019-02-17 19:38:28 +0800

    心得前端初步完成

--- Client/src/app/components/xiuxing/xinde/addxinde/addxinde.component.ts
+++ Client/src/app/components/xiuxing/xinde/addxinde/addxinde.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs'
+import { Message } from '../xinde.component';
 
 @Component({
   selector: 'app-addxinde',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddxindeComponent implements OnInit {
 
-  constructor() { }
+  public Message:Message={
+    WechatID:"asd",
+    Xinde:""
+  }
+  constructor(public http:HttpClient) { }
 
   ngOnInit() {
   }
 
+  add(){
+    console.log(this.Message);
+    if(this.Message.Xinde == ""){
+      alert("空的信息！");
+    }
+    else{
+      let api = 'https://127.0.0.1:9000';
+      const HttpOptions = {
+        headers: new HttpHeaders({'Content-Type':'application/json'})
+      }
+      this.http.post(api,this.Message,HttpOptions).subscribe(res =>{
+        console.log(res);
+      })
+    }
+  }
 }
