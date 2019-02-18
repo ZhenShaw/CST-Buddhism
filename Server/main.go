7035240c2b0c2337d62458a99b124f2c2adcd6a8
Mo <641878107@qq.com>
2019-02-18 11:50:46 +0800

    心得main函数

--- Server/main.go
+++ Server/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"./psql"
-	"./route"
 	"fmt"
 	"log"
 	"net/http"
+
+	"./psql"
+	"./route"
 )
 
 func main() {
 
 	psql.TestDB()
-
 
 	mux := http.NewServeMux()
 
@@ -23,7 +23,7 @@ func main() {
 	mux.HandleFunc("/gongfo/xiang", route.Xiang)
 	mux.HandleFunc("/gongfo/fruit", route.Fruit)
 	mux.HandleFunc("/yinjingshu", route.Yinjingshu)
-
+	mux.HandleFunc("/xiuxing/xinde", route.Xinderoute)
 
 	//监听9000端口
 	fmt.Println("Web服务器启动...端口:9000")
