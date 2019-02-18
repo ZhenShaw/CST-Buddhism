7e2180cae9afae4fa987cfea625c7b25ca4d9c2a
2226210789 <2226210789@qq.com>
2019-02-18 13:46:32 +0800

    增添数据库文件

---
+++ Sql/gongfoSQL/insert_fluit.sql
@@ -0,0 +1,9 @@
+insert into gongfo(name,url,details,status)values('苹果','http://api10.buddhaheart.cn/webfiles/sys/201710/171013142539eb07.png','',2);
+insert into gongfo(name,url,details,status)values('桔子','http://api10.buddhaheart.cn/webfiles/sys/201710/171013142642a781.png','',2);
+insert into gongfo(name,url,details,status)values('香蕉','http://api10.buddhaheart.cn/webfiles/sys/201710/171013143340492e.png','',2);
+insert into gongfo(name,url,details,status)values('菠萝','http://api10.buddhaheart.cn/webfiles/sys/201710/171013143405bc93.png','',2);
+insert into gongfo(name,url,details,status)values('桃子','http://api10.buddhaheart.cn/webfiles/sys/201710/1710131434223ca3.png','',2);
+insert into gongfo(name,url,details,status)values('人参果','http://api10.buddhaheart.cn/webfiles/sys/201710/1710131434396051.png','',2);
+insert into gongfo(name,url,details,status)values('释迦果','http://api10.buddhaheart.cn/webfiles/sys/201710/1710131434514de6.png','',2);
+
+
