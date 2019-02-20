2e10cf79daba6fea1b32fe27194b4d1df8e02296
fanyin <ly13692818869@gmail.com>
2019-02-20 17:33:57 +0800

    八字结果

--- Server/route/result.go
+++ Server/route/result.go
@@ -14,24 +14,29 @@ func Result(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
 	w.Header().Set("content-type", "application/json")             //返回数据格式是json
 	r.ParseForm()
-	fmt.Println("收到客户端请求: ", r.Method)
-	result, _ := ioutil.ReadAll(r.Body)
-	r.Body.Close()
-	fmt.Printf("%s\n", result)
-	var f interface{}
-	json.Unmarshal(result, &f)
-	m := f.(map[string]interface{})
-	var yinli string
-	for k, v := range m {
-		switch vv := v.(type) {
-		case string:
-			fmt.Println(k, "is string", vv)
-			yinli = vv
+	if r.Method == "POST" {
+		fmt.Println("收到客户端请求: ", r.Method)
+		result, _ := ioutil.ReadAll(r.Body)
+		r.Body.Close()
+		fmt.Printf("%s\n", result)
+		var f interface{}
+		json.Unmarshal(result, &f)
+		m := f.(map[string]interface{})
+		var yinli string
+		for k, v := range m {
+			switch vv := v.(type) {
+			case string:
+				fmt.Println(k, "is string", vv)
+				yinli = vv
+			}
 		}
+		var baziresult psql.Result
+		baziresult = psql.Getresult(yinli)
+		var b string
+		b = baziresult.NianMing + " " + baziresult.TianYun + " " + baziresult.ShengXiao + " " + baziresult.BenMing + " " + baziresult.MingGua
+		fmt.Println(b)
+		bb, err := json.Marshal(b)
+		Checkerr(err)
+		w.Write([]byte(bb))
 	}
-	var baziresult psql.Result
-	psql.Getresult(yinli)
-	var b string
-	b = baziresult.NianMing + baziresult.TianYun + baziresult.ShengXiao + baziresult.BenMing + baziresult.MingGua
-	fmt.Fprintf(w, b)
 }
