cf6a45b42f79484f337e9db8eb96bc0311c19f62
ZhenShaw <1614570038@qq.com>
2019-02-22 00:02:42 +0800

    修改app component

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -9,5 +9,13 @@ export class AppComponent {
   constructor() {
     // 设置body高度为窗口高度
     document.body.style.height = String(window.innerHeight) + "px"
+    // var agent = navigator.userAgent.toLowerCase()
+    // if (/iphone|ipod|ipad|ios|android/.test(agent)) {
+    //   document.body.style.height = String(window.innerHeight) + "px"
+    // } else {
+    //   console.log(agent)
+    //   document.body.style.width = "375px"
+    //   document.body.style.height = "650px"
+    // }
   }
 }
