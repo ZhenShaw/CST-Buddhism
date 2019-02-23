4513a318a2161186c5f360385f24dc2758facc9f
2226210789 <2226210789@qq.com>
2019-02-23 17:20:41 +0800

    完善录音前端

--- Client/src/app/components/gongfo/qifu/qifu.component.ts
+++ Client/src/app/components/gongfo/qifu/qifu.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-qifu',
   templateUrl: './qifu.component.html',
@@ -7,83 +7,128 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QifuComponent implements OnInit {
 
-  constructor() { }
-  // util functions
-  sleep = (ms = 0) => new Promise(resolve => setTimeout(resolve, ms));
-  log = str => (document.getElementById('logDiv') as HTMLInputElement).value += (str + '\n');
-  // rawAudioBuffer
-  rawAudioBuffer = null;
-  // convertedAudioBuffer
-  convertedAudioBuffer = null;
-  startButton = async () => {
-    const startButton = document.getElementById('startButtonid');
-    const config = {
-      numOfChannels: 1,
-      length: 3 * 44100,
-      sampleRate: 44100,
-    };
-
-    startButton.textContent = '正在录音...';
-
-    const ctx = new OfflineAudioContext(config.numOfChannels, config.length, config.sampleRate);
-
-    const source = ctx.createOscillator();
-    source.connect(ctx.destination);
-    source.start();
-
-    await this.sleep(3000);
-
-    this.rawAudioBuffer = await ctx.startRendering();
-    source.stop();
-    this.log(`rawAudioBuffer 就绪: length=${this.rawAudioBuffer.length} duration=${this.rawAudioBuffer.duration} numberOfChannels=${this.rawAudioBuffer.numberOfChannels} sampleRate=${this.rawAudioBuffer.sampleRate}`);
-    startButton.textContent = '开始录音';
-  };
-
-  convertButton = async () => {
-    const convertButton = document.getElementById('convertButtonid');
-    const config = {
-      numOfChannels: 1,
-      length: 3 * 16000,
-      sampleRate: 16000,
-    };
-
-    convertButton.textContent = '正在转换...';
-
-    const ctx = new OfflineAudioContext(config.numOfChannels, config.length, config.sampleRate);
-
-    const source = ctx.createBufferSource();
-    source.buffer = this.rawAudioBuffer;
-    source.connect(ctx.destination);
-    source.start();
-
-    await this.sleep(3000);
-
-    this.convertedAudioBuffer = await ctx.startRendering();
-    source.stop();
-    this.log(`convertedAudioBuffer 就绪: length=${this.convertedAudioBuffer.length} duration=${this.convertedAudioBuffer.duration} numberOfChannels=${this.convertedAudioBuffer.numberOfChannels} sampleRate=${this.convertedAudioBuffer.sampleRate}`);
-    convertButton.textContent = '转换录音(44100 => 16000)';
-  };
-
-  // replay
-  replayButton = async () => {
-    const ctx = new AudioContext();
-
-    const source = ctx.createBufferSource();
-    source.buffer = this.rawAudioBuffer;
-    source.connect(ctx.destination);
-    source.start();
-  };
-
-  replayConvertedButton= async () => {
-    const ctx = new AudioContext();
-
-    const source = ctx.createBufferSource();
-    source.buffer = this.convertedAudioBuffer;
-    source.connect(ctx.destination);
-    source.start();
-  };
+  constructor(private render2: Renderer2) { }
   ngOnInit() {
+    this.mounted();
+  }
+  public chunks = []
+  public chunkList = {
+    duration: 0,
+    stream: ''
+  }
+  public btnText = '按住说话'
+  ishide = true
+  recorder: any
+  audio: any
+  blobEvent: any
+  requestAudioAccess() {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+      this.recorder = new (<any>window).MediaRecorder(stream);
+      this.bindEvents();
+    }, error => {
+      alert('出错，请确保已允许浏览器获取录音权限');
+    });
   }
 
-  
+  timeOutEvent = 0;//定时器   
+
+
+  //开始按   
+  gtouchstart() {
+    document.getElementById('btntext').innerHTML = '松开结束';
+    this.timeOutEvent = window.setTimeout(() => {//此处用箭头函数对变量赋值
+      this.timeOutEvent = 0;
+      //执行长按要执行的内容，如弹出菜单   
+      console.log("长按事件触发发");
+      this.onStart();
+    },
+      500);//这里设置定时器，定义长按500毫秒触发长按事件，时间可以自己改，个人感觉500毫秒非常合适   
+    document.addEventListener('contextmenu', function (e) {//阻止浏览器默认行为，这里阻止长按弹窗
+      e.preventDefault();
+    });
+
+  };
+  //手释放，如果在500毫秒内就释放，则取消长按事件，此时可以执行onclick应该执行的事件   
+  gtouchend() {
+    document.getElementById('btntext').innerHTML = '开始录音';
+    this.onStop();
+    clearTimeout(this.timeOutEvent);//清除定时器  
+    if (this.timeOutEvent != 0) {
+      //这里写要执行的内容（尤如onclick事件）   
+      console.log("你这是点击，不是长按");
+    }
+    this.ishide = false
+  };
+  //如果手指有移动，则取消所有事件，此时说明用户只是要移动而不是长按   
+  gtouchmove() {
+    clearTimeout(this.timeOutEvent);//清除定时器   
+  };
+
+
+  onStart() {
+    this.recorder.start();
+  }
+
+  onStop() {
+    this.recorder.stop();
+  }
+
+  play() {
+    console.log(this.chunkList)
+    const audio = this.render2.selectRootElement("#audio");
+    audio.src = this.chunkList.stream;
+    audio.play();
+    var x = document.getElementsByClassName("span");
+    for(let i=0;i<3;i++){
+      (<HTMLElement>x[i]).style.animation = "wink "+1+"s ease infinite";
+      window.setTimeout(()=>{(<HTMLElement>x[i]).style.animationPlayState="running"},0);
+      window.setTimeout(()=>{(<HTMLElement>x[i]).style.animationPlayState="paused"},this.chunkList.duration*1000-1);
+    }  
+  }
+
+  bindEvents() {
+
+    this.recorder.ondataavailable = (e) => {
+      this.chunks.push(e.data);
+      console.log(e)
+      console.log(this.chunks)
+    };
+
+    this.recorder.onstop = () => {
+      console.log(this.chunks)
+      //let blob = new Blob(this.chunks, { 'type': 'audio/ogg; codecs=opus' }),
+      let blob = new Blob(this.chunks, { 'type': 'audio/mpeg; codecs=opus' }),
+        audioStream = URL.createObjectURL(blob),
+        //估算时长
+        duration = Math.round(blob.size / 6600);
+      console.log(blob)
+      if (duration <= 0) {
+        //alert('说话时间太短');
+        return;
+      }
+      if (duration > 60) {
+        duration = 60;
+      }
+      this.chunkList = { duration: duration, stream: audioStream };
+      this.chunks = [];
+      document.getElementById('voiceposition').style.width = (this.chunkList.duration * 20 + 30) + 'px';//更新录音框外层div的宽度
+      document.getElementById('time').innerHTML = String(this.chunkList.duration) + '"';//更新录音时长
+      document.getElementById('voice').style.width = this.chunkList.duration * 20 + 'px';//更新录音框宽度
+      console.log(this.chunkList);
+
+    };
+  }
+  mounted() {
+    if (!navigator.mediaDevices) {
+      alert('您的浏览器不支持获取用户设备');
+      return;
+    }
+    if (!(<any>window).MediaRecorder) {
+
+      alert('您的浏览器不支持录音');
+      return;
+    }
+    this.audio = this.render2.selectRootElement("#audio");
+    this.requestAudioAccess();
+  }
 }
