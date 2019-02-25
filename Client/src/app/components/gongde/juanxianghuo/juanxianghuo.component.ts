77a30b5c0b7d0484d03179ee398eaf5a88a8bcad
Head <1105652324@qq.com>
2019-02-25 12:34:51 +0800

    捐香火前端完善

--- Client/src/app/components/gongde/juanxianghuo/juanxianghuo.component.ts
+++ Client/src/app/components/gongde/juanxianghuo/juanxianghuo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient,HttpHeaders} from '@angular/common/http';
 @Component({
   selector: 'app-juanxianghuo',
   templateUrl: './juanxianghuo.component.html',
@@ -7,9 +7,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JuanxianghuoComponent implements OnInit {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
 
   ngOnInit() {
   }
-
+  //record用于记录用户选择的香的类型和购买的数量
+  public record1:any={
+    name:'',
+    number:'',
+    type:'',
+  }
+//选择香的按钮被点击，给香的type赋值对应的香名
+pinganxiang(){
+this.record1.type='平安香';
+console.log(this.record1.type);
+console.log(this.record1.name);
+}
+xiaozaixiang(){
+  this.record1.type='消灾香';
+}
+fuguixiang(){
+  this.record1.type='富贵香';
+}
+qiuzixiang(){
+  this.record1.type='求子香';
+}
+zengcaixiang(){
+  this.record1.type='增财香';
+}
+zhihuixiang(){
+  this.record1.type='智慧香';
+}
+//select框选择时将数值赋值给record.number
+getChange(uid:string){
+  console.log('=====');
+  console.log(uid);
+  this.record1.number=uid;
+  console.log(this.record1.number);
+}
+//后端还未完成
+pay(){
+  console.log(this.record1);              
+  const httpOptions ={headers:new HttpHeaders({'Content-Type':'application/json'})};
+  var api="http://localhost:9000/gongde/juanxianghuo";
+  this.http.post(api,this.record1,httpOptions).subscribe((response:any)=>{
+    console.log(response)
+    //判断姓名是否已经填写，香是否已选择
+   if (response=="true"){
+     alert("支付成功")
+   }
+   else{
+     alert("请填写您的名字及选择香型")
+   }
+  }) 
+  //清空消息
+  this.record1={};
+}
 }
