fd1632a3166e914ddfcfabf1ab2767b68a640520
ZhenShaw <1614570038@qq.com>
2019-03-03 17:31:56 +0800

    佛像简单上色参考

--- Client/src/app/components/welcome/welcome.component.ts
+++ Client/src/app/components/welcome/welcome.component.ts
@@ -36,7 +36,14 @@ export class WelcomeComponent implements OnInit {
       camera.attachControl(canvas, true);
 
       // 导入3D网格素材，第一个参数 "" 表示导入的所有网格，rootUrl、sceneFilename
-      BABYLON.SceneLoader.ImportMesh("", "/assets/babylon/", "buddhism.babylon", scene);
+      BABYLON.SceneLoader.ImportMesh("", "/assets/babylon/", "buddhism.babylon", scene, function (newMeshes) {
+        // 获取返回的网格
+        var buddhism = newMeshes[0];
+        // 简单上色参考
+        var Material = new BABYLON.StandardMaterial("groundMaterial", scene);
+        Material.diffuseColor = new BABYLON.Color3(1, 0.9, 0.1);
+        buddhism.material = Material;
+      });
 
       return scene;
     };
