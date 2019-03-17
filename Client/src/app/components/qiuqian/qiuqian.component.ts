c8104530f00334d34b34d19fd0f79c0394fd2498
tashicyq <15920170089@163.com>
2019-03-17 23:10:33 +0800

    求签更新(换图片+添加声音)

--- Client/src/app/components/qiuqian/qiuqian.component.ts
+++ Client/src/app/components/qiuqian/qiuqian.component.ts
@@ -8,14 +8,36 @@ import { Router, UrlSerializer } from '@angular/router';
 })
 export class QiuqianComponent implements OnInit {
 
-  constructor(public router:Router) { }
+  constructor(public router:Router) { 
+
+    this._audio1=document.createElement("audio");
+    this._audio1.src=this. playData1.Url;
+    this._audio2=document.createElement("audio");
+    this._audio2.src=this. playData2.Url;
+
+  }
   public kongzhidonghuaxiaoguo:any=''//控制动画样式
+
+  public _audio1:HTMLAudioElement;
+  public playData1={
+    Url: '../../../../assets/qiuqian/yaoyiyao.mp3'
+  }
+  public _audio2:HTMLAudioElement;
+  public playData2={
+    Url: '../../../../assets/qiuqian/jieguo.mp3'
+  }
 
   ngOnInit() {
   }
   jieqian(){
+
+    var element:any=document.getElementById('img');
+    element.src="assets/qiuqian/qian.gif";
     this.kongzhidonghuaxiaoguo='1'
-    setTimeout(() => { this.router.navigate(['jieqian']) },2000)
+    this._audio1.play();
+    setTimeout(() => { this.router.navigate(['jieqian']);
+    this._audio2.play();
+  },1500)
     //2s动画，然后跳到解签界面
   }
 
